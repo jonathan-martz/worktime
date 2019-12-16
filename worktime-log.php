1c01be9ac3228271ee6772837c02c6c734c4558d
Jonathan Martz <jonathan.martz@flagbit.de>
2019-12-16 16:55:13 +0100

    fix missing index

--- worktime-log.php
+++ worktime-log.php
@@ -19,21 +19,26 @@ $data = [
 foreach($commands as $command){
     $command = explode(' ', $command);
 
-    $project = explode('PhpstormProjects/', $command[29])[1];
 
-    $branch = exec('cd '. $command[29].' && git rev-parse --abbrev-ref HEAD');
+    if(!empty($command[29])){
+        $project = explode('PhpstormProjects/', $command[29])[1];
 
-    $data[] = [
-        'project' => $project,
-        'branch' => $branch
-    ];
+        $branch = exec('cd '. $command[29].' && git rev-parse --abbrev-ref HEAD');
+
+        $data[] = [
+            'project' => $project,
+            'branch' => $branch
+        ];
+    }
 }
+
+$json = json_encode($data, true);
 
 $ch = curl_init();
 curl_setopt($ch, CURLOPT_USERAGENT, 'worktime-logger');
 curl_setopt($ch, CURLOPT_POST,           1 );
 curl_setopt($ch, CURLOPT_URL, "https://api.jmartz.de");
-curl_setopt($ch, CURLOPT_POSTFIELDS,     json_encode($data, true) );
+curl_setopt($ch, CURLOPT_POSTFIELDS,     $json );
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 $output = curl_exec($ch);
 curl_close($ch);
