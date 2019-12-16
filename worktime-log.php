e0408e7b41dc65e368d3aa6e4752c69e5b4194d6
Jonathan Martz <jonathan.martz@flagbit.de>
2019-12-16 17:16:44 +0100

    add config with logic

--- worktime-log.php
+++ worktime-log.php
@@ -1,6 +1,11 @@
 <?php
 
-$phpstorm = exec('ps -aux | grep -i bin/phpstorm.sh');
+$filename = 'config.json';
+$file = file_get_contents($filename);
+
+$config = json_decode($file, JSON_FORCE_OBJECT);
+
+$phpstorm = exec('ps -aux | grep -i '.$config['filter']);
 $locked = exec('gnome-screensaver-command -q | grep "is active"');
 
 if(empty($locked)){
@@ -19,9 +24,8 @@ $data = [
 foreach($commands as $command){
     $command = explode(' ', $command);
 
-
     if(!empty($command[29])){
-        $project = explode('PhpstormProjects/', $command[29])[1];
+        $project = explode($config['folder']. '/', $command[29])[1];
 
         $branch = exec('cd '. $command[29].' && git rev-parse --abbrev-ref HEAD');
 
@@ -37,7 +41,7 @@ $json = json_encode($data, true);
 $ch = curl_init();
 curl_setopt($ch, CURLOPT_USERAGENT, 'worktime-logger');
 curl_setopt($ch, CURLOPT_POST,           1 );
-curl_setopt($ch, CURLOPT_URL, "https://api.jmartz.de");
+curl_setopt($ch, CURLOPT_URL, $config['api']);
 curl_setopt($ch, CURLOPT_POSTFIELDS,     $json );
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 $output = curl_exec($ch);
