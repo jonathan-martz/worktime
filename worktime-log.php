5ca7680ed92c7275bf97b72b245e3b68149cbcca
Jonathan Martz <jonathan.martz@flagbit.de>
2019-12-16 16:11:07 +0100

    add logic for curl call to api

--- worktime-log.php
+++ worktime-log.php
@@ -4,6 +4,22 @@ $output = exec('ps -aux | grep -i bin/phpstorm.sh');
 
 $commands = explode(PHP_EOL, $output);
 
+$data = [];
+
 foreach($commands as $command){
-    var_dump($command);
+    $command = explode(' ', $command);
+
+    $project = explode('PhpstormProjects/', $command[29])[1];
+
+    $data[] = [
+        'project' => $project
+    ];
 }
+
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_POST,           1 );
+curl_setopt($ch, CURLOPT_URL, "https://api.jmartz.de");
+curl_setopt($ch, CURLOPT_POSTFIELDS,     json_encode($data, true) );
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+$output = curl_exec($ch);
+curl_close($ch);
