5c8389d153fc09435b822f16a44a954d839a4d9c
Jonathan Martz <jonathan.martz@flagbit.de>
2019-12-19 16:51:33 +0100

    enable curl send

--- worktime-log.php
+++ worktime-log.php
@@ -87,5 +87,5 @@ curl_setopt($ch, CURLOPT_POST,           1 );
 curl_setopt($ch, CURLOPT_URL, $config['api']);
 curl_setopt($ch, CURLOPT_POSTFIELDS,     $json );
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-// $output = curl_exec($ch);
+$output = curl_exec($ch);
 curl_close($ch);
